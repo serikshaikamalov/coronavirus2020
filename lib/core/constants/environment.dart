a8395292555ba06214be1170b8dd8dec90dad397
Serik <serik.shaikamalov@ntpayments.com>
2020-07-11 11:41:52 +0600

    env

--- lib/core/constants/environment.dart
+++ lib/core/constants/environment.dart
@@ -1,4 +1,7 @@
 Map<String, dynamic> environement = {
   // Dev: Home
-  'apiUrl': 'http://192.168.0.104:8000',
+  // 'apiUrl': 'http://192.168.0.104:8000',
+
+  // Prod
+  'apiUrl': 'http://138.68.64.129:9000',
 };
